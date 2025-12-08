bbea3c137f8f0a8dc803b436c24b9f053b00002d
Peter Conti <petertconti@gmail.com>
2025-12-07 23:19:14 -0500

    hoist pnpm node_modules to solve lambda symlinks issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  outputFileTracingIncludes: {
-    "/*": [
-      "./node_modules/@swc/helpers/**/*",
-      "./node_modules/styled-jsx/**/*",
-    ],
-  },
 };
 
 export default nextConfig;
